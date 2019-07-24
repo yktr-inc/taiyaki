ddd309a3de221a540a92051aa2e666cd4eeebcdf
Hugo Knorr <hugo.knorr@gmail.com>
2019-07-24 12:24:05 +0200

    :sparkles: prevent to be listed in user query

--- api/app/routes/user.js
+++ api/app/routes/user.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router
   .get('/', function(req, res) {
-    User.find().select({ id: 1, username: 1 }).then(data => res.json(data));
+    User.find({ _id: { $ne: req.user._id } }).select({ id: 1, username: 1 }).then(data => res.json(data));
   });
 
 module.exports = router;
